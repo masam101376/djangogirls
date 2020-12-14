3e8b96f1d847760416e22688a88cb9ae1186f498
masam101376 <ma.sa.m10.1.3.7.6@gmail.com>
2020-12-14 15:39:41 +0900

    Add docstring of post_list function

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 
 def post_list(request):
-    """
-    docstring
+    """Postの一覧を表示する関数
+    
+    models.pyに定義した、ポストの内容を保存するためのモデルPostにあるものを表示する
     """
     return render(request, 'blog/post_list.html', {})
