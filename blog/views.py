3f9cb16882c982831aef2c240980250cd486a92d
masam101376 <ma.sa.m10.1.3.7.6@gmail.com>
2020-12-14 15:52:11 +0900

    Make render refer to the query set 'posts'

--- blog/views.py
+++ blog/views.py
@@ -9,5 +9,5 @@ def post_list(request):
     
     models.pyに定義した、ポストの内容を保存するためのモデルPostにあるものを表示する
     """
-    post = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts', posts})
